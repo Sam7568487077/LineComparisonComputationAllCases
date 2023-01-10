ef574e4d316511353a55057002c631d4b8a7e91a
Sam K Varghese <ksam071@gmail.com>
2023-01-10 11:28:24 +0530

    Checking equality of 2 lines

--- src/LineComparisonComputation.java
+++ src/LineComparisonComputation.java
@@ -13,8 +13,28 @@ public class LineComparisonComputation {
         double y1 = sc.nextDouble();
         System.out.print("Enter the second coordinate of y :");
         double y2 = sc.nextDouble();
-        double lengthOfLine = Math.sqrt( Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
         System.out.println("\n");
-        System.out.println("The length of line is: " + lengthOfLine);
+        System.out.print("Enter the first coordinate of p :");
+        double p1 = sc.nextDouble();
+        System.out.print("Enter the second coordinate of p :");
+        double p2 = sc.nextDouble();
+        System.out.print("Enter the first coordinate of q :");
+        double q1 = sc.nextDouble();
+        System.out.print("Enter the second coordinate of q :");
+        double q2 = sc.nextDouble();
+        double lengthOfLine1 = Math.sqrt( Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
+        double lengthOfLine2 = Math.sqrt( Math.pow((p2 - p1),2) + Math.pow((q2 - q1),2));
+        System.out.println("\n");
+        System.out.println("The length of line XY is: " + lengthOfLine1);
+        System.out.println("The length of line PQ is: " + lengthOfLine2);
+
+        if( lengthOfLine1 == lengthOfLine2 )
+        {
+            System.out.println("Therefore the length of the two lines are equal");
+        }
+        else {
+            System.out.println("Therefore the length of the two lines are not equal");
+        }
+
     }
 }
